0555a199744db07b515959b130773192bb03a8d6
Busanz <buddhisanz@gmail.com>
2025-11-18 13:23:43 +0100

    Birds page responsive design

--- scripts/birds.js
+++ scripts/birds.js
@@ -22,7 +22,7 @@ function Birds(
 
 const birdCassowary = new Birds(
   'Cassowary',
-  'It is not hard to imagine that cassowaries are descended from dinosaur ancestors. The largest cassowaries can stand as high as six feet and weigh up to 160 pounds. These large birds cannot fly, but their extremely powerful legs propel them at great speeds. They are strong swimmers and can move quickly on both land and water. Cassowaries are shy and they are usually hard to spot, at least in their natural rain forest habitats. They are not overly aggressive, and attacks are rare. But they can do a lot of damage if they are provoked or angered. Cassowary attacks have occasionally been deadly, including a recent one which occurred in 2019, at a private collection of caged birds in Florida',
+  'It is not hard to imagine that cassowaries are descended from dinosaur ancestors. The largest cassowaries can stand as high as six feet and weigh up to 160 pounds. These large birds cannot fly, but their extremely powerful legs propel them at great speeds. They are strong swimmers and can move quickly on both land and water. Cassowaries are shy and they are usually hard to spot, at least in their natural rain forest habitats. They are not overly aggressive, and attacks are rare. | But they can do a lot of damage if they are provoked or angered. Cassowary attacks have occasionally been deadly, including a recent one which occurred in 2019, at a private collection of caged birds in Florida',
   'Bird',
   '20 Years',
   'Plants matter like fruit, insects and small animals like mice and lizards',
@@ -33,7 +33,7 @@ const birdCassowary = new Birds(
 );
 const birdKookaburra = new Birds(
   'Kookaburra',
-  'Native to the eucalyptus forests of eastern Australia, the laughing kookaburra is the largest member of the Kingfisher family, with females weighing up to one pound and growing to 43 cm in length. Its beak can reach 10 cm long and is used to snatch a variety of invertebrates and small vertebrates, including the occasional small snake. Since being introduced in western Australia and New Zealand, the kookaburra has angered farmers by preying on their fowl. The laughing kookaburra has dark brown wing plumage and a white head and underside. Dark brown eye stripes run across its face and its upper bill is black. Its reddish-colored tail is patterned with black bars.',
+  'Native to the eucalyptus forests of eastern Australia, the laughing kookaburra is the largest member of the Kingfisher family, with females weighing up to one pound and growing to 43 cm in length. Its beak can reach 10 cm long and is used to snatch a variety of invertebrates and small vertebrates, including the occasional small snake. Since being introduced in western Australia and New Zealand, the kookaburra has angered farmers by preying on their fowl. | The laughing kookaburra has dark brown wing plumage and a white head and underside. Dark brown eye stripes run across its face and its upper bill is black. Its reddish-colored tail is patterned with black bars.',
   'Bird',
   '20 Years',
   'Insects and small animals including snakes, frogs and lizards',
@@ -44,7 +44,7 @@ const birdKookaburra = new Birds(
 );
 const birdBlackCockatoo = new Birds(
   'Black Cockatoo',
-  'It has a short crest on the top of its head. Its plumage is mostly brownish black and it has prominent yellow cheek patches and a yellow tail band. The body feathers are edged with yellow giving a scalloped appearance. The adult male has a black beak and pinkish-red eye-rings, and the female has a bone-colored beak and grey eye-rings. In flight, yellow-tailed black cockatoos flap deeply and slowly, with a peculiar heavy fluid motion. Their loud, wailing calls carry for long distances. The whiteae is found south of Victoria to the East of South Australia and is smaller in size. The yellow-tailed black cockatoo is found in temperate forests and forested areas across south and central eastern Queensland to southeastern South Australia, including a very small population persisting in the Eyre Peninsula.[4] Two subspecies are recognized, although Tasmanian and southern mainland populations of the southern subspecies xanthanotus may be distinct enough from each other to bring the total to three. Birds of subspecies funereus (Queensland to eastern Victoria) have longer wings and tails and darker plumage overall, while those of xanthanotus (western Victoria, South Australia and Tasmania) have more prominent scalloping.',
+  'It has a short crest on the top of its head. Its plumage is mostly brownish black and it has prominent yellow cheek patches and a yellow tail band. The body feathers are edged with yellow giving a scalloped appearance. The adult male has a black beak and pinkish-red eye-rings, and the female has a bone-colored beak and grey eye-rings. In flight, yellow-tailed black cockatoos flap deeply and slowly, with a peculiar heavy fluid motion. | Their loud, wailing calls carry for long distances. The whiteae is found south of Victoria to the East of South Australia and is smaller in size. The yellow-tailed black cockatoo is found in temperate forests and forested areas across south and central eastern Queensland to southeastern South Australia, including a very small population persisting in the Eyre Peninsula.[4] Two subspecies are recognized, although Tasmanian and southern mainland populations of the southern subspecies xanthanotus may be distinct enough from each other to bring the total to three. Birds of subspecies funereus (Queensland to eastern Victoria) have longer wings and tails and darker plumage overall, while those of xanthanotus (western Victoria, South Australia and Tasmania) have more prominent scalloping.',
   'Bird',
   '41 Years',
   ' Fruit, seeds and other plant material',
@@ -107,8 +107,11 @@ const displayWelcomeContent = () => {
   const contentTitle = document.createElement('h2');
   const contentParagraph = document.createElement('p');
   contentTitle.textContent = 'Welcome to the birds section';
-  contenWrapper.appendChild(contentTitle);
   contentParagraph.textContent = contentWelcomeDescription;
+  contentTitle.style.padding = '15px 0';
+  contentParagraph.style.padding = '15px 0';
+  contentParagraph.style.maxWidth = '1000px';
+  contenWrapper.appendChild(contentTitle);
   contenWrapper.appendChild(contentParagraph);
   content.appendChild(contenWrapper);
 };
@@ -132,8 +135,8 @@ const displayContent = (index) => {
   birdImage.style.padding = '18px 0';
 
   birdDescription.textContent = !seemore
-    ? birdContent[index].description.slice(0, 500)
-    : birdContent[index].description;
+    ? birdContent[index].description.split('|')[0]
+    : birdContent[index].description.split('|').join(' ');
 
   if (seemore) {
     const seemoreDetails = [
